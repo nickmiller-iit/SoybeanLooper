7b65a5ad0e41b433ed99911c0420da9178e85a4d
Nick Miller <nmiller11@iit.edu>
2018-05-22 16:03:55 -0500

    script to remove pip chars from fasta ids

---
+++ python/fastaRemovePipes.py
@@ -0,0 +1,20 @@
+# Ad-hoc script to remove pipe characters from identifiers in a fasta file
+#
+# Requires Biopython
+#
+# As a bonus, converts fasta files to 2-line format
+
+import sys
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
+infileName = sys.argv[1]
+
+inFile = SeqIO.parse(infileName, "fasta")
+
+for record in inFile:
+#    record.id = record.id.split("|")[0]
+#    record.id = None
+    SeqIO.write(SeqRecord(record.seq, id = record.id.split("|")[0], description = ""), sys.stdout, "fasta-2line")
+
+
